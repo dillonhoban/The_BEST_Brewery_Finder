dde2d33104e64697aab306fc508fc67ad506aa42
Dillon H <dhobs21@gmail.com>
2020-11-10 20:23:58 -0800

    deleted extra js file and udpated readme

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
 
-    //var citySearch = "san_diego"
-
     $("#brew-search-button").on("click", function () {
         //get the user input
         var userInput = $("#brew-input-box").val().trim();
@@ -9,7 +7,6 @@ $(document).ready(function () {
         var settings = {
             "url": "https://api.openbrewerydb.org/breweries?per_page=5&by_city=" + userInput,
             "method": "GET",
-
         };
         // First API call to get brewery information based on city
         $.ajax(settings).done(function (response) {
@@ -17,20 +14,60 @@ $(document).ready(function () {
         });
     });
 
-    // // Second API call
-    //     var website = "https://www.stonebrewing.com/";
-    //     var apiKey = "09075edad94b73d2e456701b0a9d5e20";
-    //     $.ajax({
-    //         url: "https://api.linkpreview.net?key=" + apiKey + "&q=" + website,
-    //         success: function (result) {
-    //             console.log(result);
-    //             // Appends response data to HMTL page
-    //             var imgURL = result.image;
-    //             // Creating an element to hold the image
-    //             var image = $("<img>").attr("src", imgURL);
-    //              // Appending the image
-    //             $("#empty-div").append(image);
-    //         }
-    //     });
+    // Second API call
+    var website = "https://www.stonebrewing.com/";
+    var apiKey = "09075edad94b73d2e456701b0a9d5e20";
+    $.ajax({
+        url: "https://api.linkpreview.net?key=" + apiKey + "&q=" + website,
+        success: function (result) {
+            console.log(result);
+            // Appends response data to HMTL page
+            var imgURL = result.image;
+            // Creating an element to hold the image
+            var image = $("<img>").attr("src", imgURL);
+            // Appending the image
+            $("#empty-div").append(image);
+        }
+    });
+
+
+
+    var settings = {
+        "url": "https://api.openbrewerydb.org/breweries?per_page=5&by_city=san diego",
+        "method": "GET",
+    };
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+        $("#brewery-names").empty();
+        for (var i = 0; i < response.length; i++) {
+            var breweryName = response[i].name;
+            var breweryURL = response[i].website_url;
+            // var breweryDiv = $("<div class='breweries'>");
+            $("#brewery-names").append(breweryName);
+            $("#brewery-URLs").append(breweryURL)
+        }
+    });
+
+    $.ajax({
+        url: "https://api.linkpreview.net?key=" + apiKey + "&q=" + website,
+        success: function (result) {
+            console.log(result);
+            var title = result.title;
+            var h1El = $("<h1>").text("Title:", +title);
+            $("#empty-div").append(h1El);
+            // Appends response data to HMTL page
+            var imgURL = result.image;
+            // Creating an element to hold the image
+            var image = $("<img>").attr("src", imgURL);
+            // Appending the image
+            $("#empty-div").append(image);
+            var description = result.description;
+            var pEl = $("<p>").text("Description: " + description);
+            $("#empty-div").append(description);
+            var breweryURL = result.url;
+            var link = $("<a>").attr("href", breweryURL);
+            $("#empty-div").append(breweryURL)
+        }
+    });
 
 });
